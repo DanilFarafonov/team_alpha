271cbeafb07cccbe2961dcdd3cc33fa7d1eed38c
Danil_Farafonov <faraphonovd@yandex.ru>
2022-12-10 00:42:33 +0500

    colors output and some fixes

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#!pip install transformers scipy ftfy diffusers
-#!pip install matplotlib
-#pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-#pip install accelerate
-
 from diffusers import StableDiffusionPipeline
 import torch
 import streamlit as st
 import colour_identifier
 
 # download and save our model
-@st.cache
 def model_load():
     model_id = "prompthero/openjourney"
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
@@ -23,21 +17,20 @@ def generate_image(pipe, text):
     image.save(image_path)
     return image_path
 
-
 pipe = model_load()
 
 # title
 st.title('TEAM ALPHA')
 
 # slider, text input and play button
-color_count = st.slider("Pallete size", min_value=2, max_value=10)
+color_count = st.slider("Pallete size", min_value=1, max_value=10)
 text = st.text_input('Your image idea')
 but_make_pallete = st.button('Make Pallete')
 
 # if play button was pressed
 if but_make_pallete:
     # image generating
-    image = generate_image(text, color_count)
+    image = generate_image(pipe, text)
 
     # show image
     st.subheader('Generated Image')
@@ -47,13 +40,10 @@ if but_make_pallete:
     but_down = st.download_button('Download Image', image)
 
     # getting colors from image
-    colors = colour_identifier.colorz(image, 5)
+    colors = list(colour_identifier.colorz(image, color_count))
 
-    # print colors
-    colors_str = ''
-    for col in colors:
-        colors_str += col + ' '
-    st.text(colors_str)
-
-
-
+    # display colors
+    columns = st.columns(color_count)
+    for index in range(color_count):
+        with columns[index]:
+            st.color_picker(label=colors[index], value=colors[index], disabled=True)
