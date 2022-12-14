a847bf2d7f79248bd09f45f0e3e908b8039a51b4
Danil_Farafonov <faraphonovd@yandex.ru>
2022-12-14 22:47:26 +0500

    rename, add docstrings

--- team_alpha.py
+++ team_alpha.py
@@ -3,15 +3,22 @@ import torch
 import streamlit as st
 import colour_identifier
 
-# download and save our model
 def model_load():
+    """Downloaded and return OpenJourney model"""
+
     model_id = "prompthero/openjourney"
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
     return pipe
 
-# make new image using our model
 def generate_image(pipe, text):
+    """Generate a new image using a provided model.
+
+    Keyword arguments:
+    pipe -- provided model
+    text -- input description for image generating
+    """
+
     image = pipe(text).images[0]
     image_path = f"./{text}.png"
     image.save(image_path)
@@ -23,7 +30,7 @@ pipe = model_load()
 st.title('TEAM ALPHA')
 
 # slider, text input and play button
-color_count = st.slider("Pallete size", min_value=1, max_value=10)
+pallete_size = st.slider("Pallete size", min_value=1, max_value=10)
 text = st.text_input('Your image idea')
 but_make_pallete = st.button('Make Pallete')
 
@@ -37,10 +44,10 @@ if but_make_pallete:
     st.image(image)
 
     # getting colors from image
-    colors = list(colour_identifier.colorz(image, color_count))
+    colors = list(colour_identifier.colorz(image, pallete_size))
 
     # display colors
-    columns = st.columns(color_count)
-    for index in range(color_count):
+    columns = st.columns(pallete_size)
+    for index in range(pallete_size):
         with columns[index]:
             st.color_picker(label=colors[index], value=colors[index], disabled=True)
