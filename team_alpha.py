7c88680750ff2ca306851d89331aa52faf9a6095
stibiumghost <116355639+stibiumghost@users.noreply.github.com>
2022-12-16 13:02:46 +0500

    Update team_alpha.py
    Changes in layout

--- team_alpha.py
+++ team_alpha.py
@@ -26,28 +26,41 @@ def generate_image(pipe, text):
 
 pipe = model_load()
 
-# title
-st.title('TEAM ALPHA')
+buff, col, buff2 = st.columns([20,60,20]) # define column size
 
-# slider, text input and play button
-pallete_size = st.slider("Pallete size", min_value=1, max_value=10)
-text = st.text_input('Your image idea')
-but_make_pallete = st.button('Make Pallete')
+col.markdown('# TEAM :a:LPHA') # title
+col.markdown('### Describe your dream palette')
+text = col.text_input(label='Your prompt:', placeholder='Cold autumn morning')  # text input
+palette_size = col.slider("Palette size:", min_value=3, max_value=6, value=4) # slider
+but_make_palette = col.button('Make Palette') # play button
 
-# if play button was pressed
-if but_make_pallete:
+# when the button is pressed
+if but_make_palette and text:
     # image generating
     image = generate_image(pipe, text)
 
-    # show image
-    st.subheader('Generated Image')
-    st.image(image)
+    # get colors from the image
+    colors = list(colour_identifier.colorz(image, palette_size))
 
-    # getting colors from image
-    colors = list(colour_identifier.colorz(image, pallete_size))
+    # redefine column size based on number of colors
+    cols = [20]
+    for _ in range(palette_size):
+        cols.append(60 / palette_size)
+    cols.append(20)
 
     # display colors
-    columns = st.columns(pallete_size)
-    for index in range(pallete_size):
-        with columns[index]:
-            st.color_picker(label=colors[index], value=colors[index], disabled=True)
+    col.markdown("----", unsafe_allow_html=True)
+
+    color = 0
+    for ind, column in enumerate(st.columns(cols)):
+        if 0 < ind < len(cols) - 1:
+            with column:
+                st.color_picker(label=colors[color], value=colors[color], disabled=True)
+            color += 1
+    
+    buff, col, buff2 = st.columns([20,60,20]) # redefine column size
+    col.markdown("----", unsafe_allow_html=True)
+    
+    # show the image -- might be bad without a safety checker of some sort
+    # col.markdown('### Generated Image')
+    # col.image(image)
